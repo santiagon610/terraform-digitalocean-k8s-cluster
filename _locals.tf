b815b11f34c3e3fc1cef4ef77067be69694dbe01
Nicholas Santiago <nicholas.santiago@gmail.com>
2024-10-26 12:30:40 -0400

    cumulus/backlog#171: add chartmuseum urls to cluster bootstrap

--- _locals.tf
+++ _locals.tf
@@ -5,7 +5,10 @@ locals {
     helm_chart = "raw-2.0.0"
     helm_source_repos = [
       "https://git.coreinfra.cloud/coreinfra/argo-apps.git",
-      "https://github.com/tckgroup/infra-argo.git"
+      "https://charts.inl.io/",
+      "https://sdbx-charts.inlv2.com/",
+      "https://dev-charts.inlv2.com/",
+      "https://test-charts.inlv2.com/",
     ]
     namespace_labels = {
       managed_by = "terraform"
